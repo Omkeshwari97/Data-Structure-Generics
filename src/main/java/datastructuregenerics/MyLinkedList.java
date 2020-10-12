3b7662a0868cf034a1b7e39768ff49d7210f432d
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-10-13 00:18:36 +0530

    Commit: Initial commit

---
+++ src/main/java/datastructuregenerics/MyLinkedList.java
@@ -0,0 +1,9 @@
+package datastructuregenerics;
+
+public class MyLinkedList 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to data structure using generics");
+	}
+}
