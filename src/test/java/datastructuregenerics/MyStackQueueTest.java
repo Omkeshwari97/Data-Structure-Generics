a75a7a8dead33a3c877cd0020372795a53168ddb
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-10-14 00:33:31 +0530

    Added functionality to create queue by appending elements at end

--- src/test/java/datastructuregenerics/MyStackQueueTest.java
+++ src/test/java/datastructuregenerics/MyStackQueueTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class MyStackQueueTest {
 
-	private MyLinkedList myQueue;
+	//Stack 
 
 	@Test
 	public void givenNumberWhenPushedToStackCreateStack() 
@@ -67,6 +67,46 @@ public class MyStackQueueTest {
 		myStack.display();
 		assertEquals(0, myStack.size());
 		assertEquals(null, myStack.pop());
+	}
+	
+	//Queue
+	
+	@Test
+	public void givenNumberWhenEnqueuedToQueueToAppendAtEnd() 
+	{
+		INode front, rear;
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		
+		MyLinkedList myQueue = new MyLinkedList();
+		
+		myQueue.append(myFirstNode);
+		front = myQueue.head;
+		assertEquals(front, myFirstNode);
+		rear = myQueue.tail;
+		assertEquals(rear, myFirstNode);
+		myQueue.display();
+		
+		myQueue.append(mySecondNode);
+		front = myQueue.head;
+		assertEquals(front, myFirstNode);
+		rear = myQueue.tail;
+		assertEquals(rear, mySecondNode);
+		myQueue.display();
+		
+		myQueue.append(myThirdNode);
+		front = myQueue.head;
+		assertEquals(front, myFirstNode);
+		rear = myQueue.tail;
+		assertEquals(rear, myThirdNode);
+		myQueue.display();
+		
+		boolean result = myQueue.head.equals(myFirstNode)&&
+							myQueue.head.getNext().equals(mySecondNode)&&
+							myQueue.tail.equals(myThirdNode);
+		
+		assertTrue(result);
 	}
 	
 }
